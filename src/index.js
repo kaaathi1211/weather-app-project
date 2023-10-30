83a7c3a840ca97486b0d75601cfeb0f4d419e8cd
kaaathi1211 <148611937+kaaathi1211@users.noreply.github.com>
2023-10-30 20:40:52 +1100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,39 @@
+let now = new Date();
+let minutes = now.getMinutes();
+let hours = now.getHours();
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+let day = days[now.getDay()];
+
+let dayTimeEvent = document.querySelector(".day-time");
+dayTimeEvent.innerHTML = `${day}, ${hours}:${minutes}`;
+
+function displayWeatherCondition(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+}
+
+function showCity(event) {
+  event.preventDefault();
+  let apiKey = "0e5521dea8144981cfb14ff7e9aa06a6";
+  let city = document.querySelector("#city-input-field").value;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+let searchForm = document.querySelector("#city-input");
+searchForm.addEventListener("submit", showCity);
